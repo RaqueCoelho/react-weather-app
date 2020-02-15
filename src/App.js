eeedb77ae856a7986db30bc595225397687e1be0
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-02-15 19:02:45 +0000

    Added the GIT HUB repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This projec was coded by Raquel Coelho and is{' '}
+        <a href="https://github.com/RaqueCoelho/react-weather-app" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
