13e43fcbf94fdc73eaa7e547d3bb73cc22e7cf84
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-02-23 19:03:36 +0000

    Api integration done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This projec was coded by Raquel Coelho and is{' '}
-        <a href="https://github.com/RaqueCoelho/react-weather-app" target="_blank">
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This projec was coded by Raquel Coelho and is{' '}
+          <a href="https://github.com/RaqueCoelho/react-weather-app" target="_blank">
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import './Weather.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      date: 'Wednesday 07:00',
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+      city: response.data.name
+    });
+
+    setReady(true);
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+            autoFocus="on"
+          />
+          <input type="submit" value="Search" class="btn btn-primary w-50" />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li classNama="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img src="{weather.Data.icon}" alt="weather.description" className="float-left" />
+              <span className="temperature">{Math.round(weatherData.temperature)}</span>
+              <span className="unit">Cº</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = '26f707e504a3173f7ff66b88f084e8a0';
+    const city = 'Funchal';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return 'Loading...';
+  }
+}
