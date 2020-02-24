3bde10c7fc04fbcd85ab1b97b721864c9df74927
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-02-24 22:32:52 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import Weather from './Weather';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Funchal" />
         <footer>
           This projec was coded by Raquel Coelho and is{' '}
-          <a href="https://github.com/RaqueCoelho/react-weather-app" target="_blank">
-            open sourced on GitHub
-          </a>
+          <a href="https://github.com/RaqueCoelho/react-weather-app">open sourced on GitHub</a>
         </footer>
       </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/WeatherForecast.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import WeatherIcon from './WeatherIcon';
+import axios from 'axios';
+import './WeatherForecast.css';
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  function formatHours(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  }
+
+  if (loaded && forecast.city.name === props.city) {
+    return (
+      <div className="WeatherForecast row">
+        {forecast.list.slice(0, 5).map(function(weather) {
+          return (
+            <div className="col">
+              {formatHours(new Date(weather.dt * 1000))}
+              <WeatherIcon code={weather.weather[0].icon} />
+              {Math.round(weather.main.temp)}°C
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    const apiKey = '26f707e504a3173f7ff66b88f084e8a0';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
